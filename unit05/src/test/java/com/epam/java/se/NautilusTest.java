644a112ba46b334e11e2f289464a360d00d6afca
Valeriy Burmistrov <chrisburrrma@gmail.com>
2017-03-12 14:40:43 +0300

    empty directory catalog test

--- unit05/src/test/java/com/epam/java/se/NautilusTest.java
+++ unit05/src/test/java/com/epam/java/se/NautilusTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -14,10 +12,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class NautilusTest {
 
     @Test
-    public void testThatWeCanCreateListOfDirectoryContent() throws IOException {
+    public void testThatWeCanCreateCatalogOfDirectoryContent() throws IOException {
         final Nautilus nautilus = new Nautilus();
-        final File[] listOfContent = nautilus.ls(new File("C:\\"));
+        final File[] catalogOfContent = nautilus.ls(new File("C:\\"));
 
-        assertThat(Arrays.equals(listOfContent, new File("C:\\").listFiles()), is(true));
+        assertThat(Arrays.equals(catalogOfContent, new File("C:\\").listFiles()), is(true));
     }
+
+    @Test
+    public void testThatCatalogOfDirectoryContentReturnEmptyArrayIfDirectoryIsEmpty() {
+        final Nautilus nautilus = new Nautilus();
+        final File[] catalogOfContent = nautilus.ls(new File("C:\\test\\"));
+
+        assertThat(catalogOfContent.length, is(0));
+    }
+
+    @Test
+    public void 
 }
