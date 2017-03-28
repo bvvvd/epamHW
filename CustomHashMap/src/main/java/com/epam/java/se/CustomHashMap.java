ab0e2128b4a039d4266f6c0d29e0a9b5a5ee91dc
Valeriy Burmistrov <chrisburrrma@gmail.com>
2017-03-28 14:16:21 +0300

    CustomHashMap create test

---
+++ CustomHashMap/src/main/java/com/epam/java/se/CustomHashMap.java
@@ -0,0 +1,67 @@
+package com.epam.java.se;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class CustomHashMap<K, V> implements Map<K, V> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
---
+++ CustomHashMap/src/test/java/com/epam/java/se/CustomHashMapTest.java
@@ -0,0 +1,25 @@
+package com.epam.java.se;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CustomHashMapTest {
+
+    private Map<Integer, String> customMap;
+
+    @Before
+    public void init() {
+        customMap = new CustomHashMap<>();
+    }
+
+    @Test
+    public void testThatWeCanCreate() {
+        assertThat(customMap, is(notNullValue()));
+    }
+}
